08054cbe5425ab7c8dca2146035276e1a320592a
Luiz Almada <luizalmada@yahoo.com>
2024-03-20 01:38:14 -0300

    Integra o Firebase - cria arquivo de configuração e testa a criação de um usuário com hardcoded

--- alura-esporte/babel.config.js
+++ alura-esporte/babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+
+    // Código copiado da documentação
+    // https://www.npmjs.com/package/react-native-dotenv
+    // por Almada
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          blocklist: null,
+          allowlist: null,
+          blacklist: null, // DEPRECATED
+          whitelist: null, // DEPRECATED
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+    ],
   };
 };
--- alura-esporte/src/telas/Cadastro/index.js
+++ alura-esporte/src/telas/Cadastro/index.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import Botao from '../../componentes/Botao';
 import { EntradaTexto } from '../../componentes/EntradaTexto';
 import estilos from './estilos';
+
+import { auth } from '../../config/firebase';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 
 export default function Cadastro({ navigation }) {  
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [confirmaSenha, setConfirmaSenha] = useState('');
 
+  useEffect(() => {
+    createUserWithEmailAndPassword(auth, "luiz2@email.com", "123456")
+    .then((dadosDoUsuario) => {
+      console.log(dadosDoUsuario);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  })
+  
   return (
     <View style={estilos.container}>
       <EntradaTexto 
